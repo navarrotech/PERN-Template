d188aef0c4cb76f626056e6f9868821854694ac2
Alex Navarro <alex@navarrocity.com>
2022-12-20 20:59:08 -0700

    Update login.js

--- functions/auth/login.js
+++ functions/auth/login.js
@@ -1,5 +1,9 @@
-const database = require('scripts/Database.js')
+const database = require('../../scripts/Database.js')
 
 module.exports = function(req, res){
-    res.send({ message:"Good job" })
+    const { email, password } = req.body
+
+    if(!email || !password){ return res.status(400).send({ message: "Email and password is required in order to login!" }) }
+
+    res.status(200).send({ message:"Good job" })
 }
